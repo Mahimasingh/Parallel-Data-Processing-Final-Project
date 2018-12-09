eae44c23712a71b0608a347cfdf439760b1668cf
Mahima Singh <singh.m@husky.neu.edu>
2018-12-09 14:58:18 -0500

    Latest Page rank with which handles sink nodes

--- pagerank/src/main/scala/ccs/neu/edu/pagerank/pageRankRDD.scala
+++ pagerank/src/main/scala/ccs/neu/edu/pagerank/pageRankRDD.scala
@@ -10,6 +10,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
+import org.apache.spark.sql.SparkSession
 // import classes required for using GraphX
 import org.apache.spark.graphx._
 object pageRank {
@@ -21,28 +22,50 @@ object pageRank {
         System.exit(1)
     }
   
-  val sparkConfig = new SparkConf().setAppName("Twitter Page Rank")
-  val iteration = args(1).toInt
-  val sparkContext = new SparkContext(sparkConfig)
-  val graph = sparkContext.textFile(args(0),1)
-  val edges = graph.map{ s => 
+    val sparkConfig = new SparkConf().setAppName("Twitter Page Rank")
+    val iters = if (args.length > 1) args(1).toInt else 10
+    val sparkContext = new SparkContext(sparkConfig)
+    val graph = sparkContext.textFile(args(0),1)
+    
+    val rightEdges = graph.map( s => 
+             s.split("\\s+")(1)
+              ).distinct()
+    val leftEdges = graph.map( s => 
+             s.split("\\s+")(0)).distinct()
+   
+    val danglingEdges = rightEdges.subtract(leftEdges).map(x=> (x.toString,"dummy")).groupByKey()
+    var edges = graph.map{ s => 
               val node = s.split("\\s+")
               (node(0),node(1))
-              }.distinct().groupByKey().cache()
-  var ranks = edges.mapValues(v => 1.0)
-  for(i <- 1 to iteration) {
-    logger.info("Iterating : " + i)
-    val intermediateRDD = edges.join(ranks).values.flatMap{ case(links,rank) =>
-      val size = links.size
-      links.map(links => (links, rank / size))
-  }
-    /*
-     * Teleportating page rank : https://www.cs.princeton.edu/~chazelle/courses/BIB/pagerank.htm
-     */
-    ranks = intermediateRDD.reduceByKey(_+_).mapValues(0.15 + 0.85 * _)
-  }
+              }.distinct().groupByKey()
   
-  ranks.saveAsTextFile(args(2))
- 
+  edges = edges.union(danglingEdges)
+  var ranks = edges.mapValues(v => 1.0)
+  val dummyRank=sparkContext.parallelize(Seq(("dummy",0.0)))
+  ranks = ranks.union(dummyRank)
+  val globalRanks = ranks
+  edges.cache()
+  globalRanks.persist()
+  var d = 0.15
+    for (i <- 1 to iters) {
+      val contribs = edges.join(ranks).values.flatMap{ case (urls, rank) =>
+        val size = urls.size
+        urls.map(url => (url, (rank / size)))
+      }
+      
+      ranks = contribs.reduceByKey(_ + _).mapValues((0.15 + 0.85 * _))
+      var noIncoming = globalRanks.subtractByKey(ranks).map(x => (x._1,0.0))
+      
+      ranks = ranks.union(noIncoming)
+      var delta = ranks.lookup("dummy")(0)
+      var size = ranks.count() - 1
+    
+    ranks = ranks.map(vertex => if(vertex._1 !="dummy") (vertex._1, (vertex._2 +( delta / size.toDouble))) else (vertex._1,vertex._2))
+    
+    }
+
+    
+    ranks.saveAsTextFile(args(2))
+
   }
- }
+}
